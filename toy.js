db821d76a2eda988e0bc5812c6a36b741903f910
Dolcythareja <156478380+Dolcythareja@users.noreply.github.com>
2024-11-13 00:08:15 -0500

    Created an instance of ToyCar in toy.js

--- toy.js
+++ toy.js
@@ -13,3 +13,20 @@ function ToyCar(name, brand, color, length, width, height, weight, material, pri
     this.maxSpeed = maxSpeed;
     this.features = features;
 }
+// Create an instance of ToyCar
+const ferrari = new ToyCar(
+    "Ferrari 488 GTB",
+    "Ferrari",
+    "Red",
+    15,
+    7,
+    4,
+    200,
+    "Die-cast metal",
+    19.99,
+    false,
+    "None",
+    "3+",
+    10,
+    ["Realistic Interior", "Detailed Engine", "Rubber Tires"]
+);

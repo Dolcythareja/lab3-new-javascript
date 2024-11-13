c509d8dd3135d308408749bc0a586ed8d30e2984
Dolcythareja <156478380+Dolcythareja@users.noreply.github.com>
2024-11-13 00:07:48 -0500

    Made a constructor in toy.js

--- toy.js
+++ toy.js
@@ -1,1 +1,15 @@
-
+// ToyCar Constructor
+function ToyCar(name, brand, color, length, width, height, weight, material, price, isRemoteControlled, batteryType, ageRecommendation, maxSpeed, features) {
+    this.name = name;
+    this.brand = brand;
+    this.color = color;
+    this.dimensions = { length, width, height };
+    this.weight = weight;
+    this.material = material;
+    this.price = price;
+    this.isRemoteControlled = isRemoteControlled;
+    this.batteryType = batteryType;
+    this.ageRecommendation = ageRecommendation;
+    this.maxSpeed = maxSpeed;
+    this.features = features;
+}
